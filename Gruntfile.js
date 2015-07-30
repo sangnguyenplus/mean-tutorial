3c4c827be58cd63d9ff687ad296a3bae2885a8de
SangAntking <sangnguyenminh@antking.com.vn>
2015-07-30 21:57:38 +0700

    Add Gruntfile.js

---
+++ Gruntfile.js
@@ -0,0 +1,53 @@
+/**
+ * Created by Sang Nguyen on 30/07/2015.
+ */
+
+//Gruntfile
+module.exports = function(grunt) {
+
+
+    //Initializing the configuration object
+    grunt.initConfig({
+
+        cssmin: {
+            admin: {
+                files: {
+                    './public/css/style.min.css': ['./public/css/style.css'],
+
+                }
+            }
+        },
+        watch:{
+            less: {
+                files: [
+                    './public/css/*.css',
+                ],  //watched files
+                tasks: ['cssmin'],
+                options: {
+                    livereload: true                        //reloads the browser
+                }//tasks to run
+            },
+            livereload: {
+                files: [
+                    './public/css/*.css'
+                ],
+                options: {
+                    livereload: true
+                }
+
+            }
+
+        }
+
+    });
+
+    // // Plugin loading
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+
+    // Task definition
+    grunt.registerTask('default', ['watch']);
+    grunt.registerTask('build', ['less']);
+
+};
